ee1f056b9c2a78c0850c641c2ee120cfacc6217a
samkallon <39991473+samkallon@users.noreply.github.com>
2022-05-31 16:27:48 +0800

    feature: 添加样式

--- src/App.js
+++ src/App.js
@@ -26,6 +26,38 @@ const list = [
     points: 5,
     objectID: 2,
   },
+  {
+    title: 'microsoft',
+    url: 'Https://xxxxx',
+    author: 'sam',
+    num_comments: 3,
+    points: 5,
+    objectID: 3,
+  },
+  {
+    title: 'SDE',
+    url: 'Https://xxxxx',
+    author: 'sam',
+    num_comments: 3,
+    points: 5,
+    objectID: 4,
+  },
+  {
+    title: 'home',
+    url: 'Https://xxxxx',
+    author: 'sam',
+    num_comments: 3,
+    points: 5,
+    objectID: 5,
+  },
+  {
+    title: 'life',
+    url: 'Https://xxxxx',
+    author: 'sam',
+    num_comments: 3,
+    points: 5,
+    objectID: 6,
+  }
 ];
 const isSearched = (searchTerm) => (item) => item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
@@ -51,13 +83,16 @@ class App extends Component {
   render() {
     const {searchTerm, list} = this.state;
     return (
-      <div className="App">
-        <Search
-          value={searchTerm}
-          onChange={this.onSearchChange}
-        >
-          搜索
-        </Search>
+      <div className="page">
+        <div className="interactions">
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          >
+            搜索
+          </Search>
+        </div>
+
         <Table
           list={list}
           pattern={searchTerm}
@@ -79,19 +114,20 @@ const Search = ({value, onChange, children}) =>
   </form>
 
 const Table = ({list, pattern, onDismiss}) =>
-
-  <div>
+  <div className="table">
     {
       list.filter(isSearched(pattern)).map((item) => (
-        <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-          <span>
-                <Button onClick={() => onDismiss(item.objectID)}>
+        <div key={item.objectID} className='table-row'>
+          <span style={{width:'40%'}}>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span style={{width:'30%'}}>{item.author}</span>
+          <span style={{width:'10%'}}>{item.num_comments}</span>
+          <span style={{width:'10%'}}>{item.points}</span>
+          <span style={{width:'10%'}}>
+                <Button onClick={() => onDismiss(item.objectID)}
+                  className='button-inline'
+                >
                   Dismiss
                 </Button>
               </span>
